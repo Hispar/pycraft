6cd7659e40ff109c2f26427e97a049a1aa65b937
pburgos <pburgos@corpora360.com>
2016-04-21 18:10:28 +0200

    Formatting file

--- pycraft/gamestate.py
+++ pycraft/gamestate.py
@@ -1,95 +1,100 @@
-'''
-	Requires python version 3.4 or greater
-'''
+# Requires python version 3.4 or greater
 from enum import Enum
+
+
 class States(Enum):
-	MAIN_SCREEN
-	MAIN_MENU
-	RUNNING
-	INVENTORY_MENU
-	CRAFTING_MENU
-	OPTIONS_MENU
+    MAIN_SCREEN
+    MAIN_MENU
+    RUNNING
+    INVENTORY_MENU
+    CRAFTING_MENU
+    OPTIONS_MENU
 
 
-'''
-	Just an interface. All Game States implementation defined in the States enum should extend this class and
-	implement those methods
-'''	
 class GameState:
+    """Just an interface. All Game States implementation defined in the States enum should extend this class and
+    implement those methods
+    """
 
-	def __init__(self):
-		pass
-	
-	# --- ** --
-	'''
-		Think about those resposabilities from the GameState.
-		
-		Some game states may only need to be updated, while another only need to be drew
-	'''
-	def on_update(self):
-		pass
+    def __init__(self):
+        pass
 
-	def on_draw(self):
-		pass
-	# --- ** --
+    # --- ** --
+    """
+        Think about those resposabilities from the GameState.
 
-	'''
-		Aro those responsibilities of the Game State class?
-	'''
-	def on_input(self):
-		pass
+        Some game states may only need to be updated, while another only need to be drew
+    """
 
-	def on_click(self):
-		pass
+    def on_update(self):
+        pass
 
-	# --- ** ---
-	# Methods related to the GameStateManager
-	'''
-		To be called AFTER the game state has been placed in the GameStateManager stack
-	'''	
-	def on_entered(self):
-		pass
+    def on_draw(self):
+        pass
 
-	'''
-		To be called BEFORE the game state is removed from the game state manager
-	'''
-	def on_leaving(self):
-		pass
+    # --- ** --
 
-	'''
-		To be called BEFORE another game state is stacked on top of the actual
-	'''
-	def on_obscuring(self):
-		pass
+    """
+        Aro those responsibilities of the Game State class?
+    """
 
-	'''
-		To be called AFTER this actual game state to be on the top of the GameStateManager stacked
-	'''
-	def on_revealed(self):
-		pass
+    def on_input(self):
+        pass
 
-'''
-	Desirable option for game states switch - don't let this function for the main method
-	
-	It's implemented as a stack in order to let game states return to previous one without 
-	the need it to know for which state it is returning to (e.g. maps and option menus)
-'''
+    def on_click(self):
+        pass
+
+    # --- ** ---
+    # Methods related to the GameStateManager
+    """
+        To be called AFTER the game state has been placed in the GameStateManager stack
+    """
+
+    def on_entered(self):
+        pass
+
+    """
+        To be called BEFORE the game state is removed from the game state manager
+    """
+
+    def on_leaving(self):
+        pass
+
+    """
+        To be called BEFORE another game state is stacked on top of the actual
+    """
+
+    def on_obscuring(self):
+        pass
+
+    """
+        To be called AFTER this actual game state to be on the top of the GameStateManager stacked
+    """
+
+    def on_revealed(self):
+        pass
+
+
 class GameStateManager:
+    """
+    Desirable option for game states switch - don't let this function for the main method
 
-	def __init__(self):
-		self.stack = list()
+    It's implemented as a stack in order to let game states return to previous one without
+    the need it to know for which state it is returning to (e.g. maps and option menus)
+    """
 
-	# TODO define a good way to map each possible game state
-	def switch_game_state(self):
-		pass
+    def __init__(self):
+        self.stack = list()
 
-	def peek(self):
-		return self.stack[len(self.stack)-1]
+    # TODO define a good way to map each possible game state
+    def switch_game_state(self):
+        pass
 
-	def pop(self):
-		return self.stack.pop()
+    def peek(self):
+        return self.stack[len(self.stack) - 1]
 
-	def push(self, game_state):
-		self.stack.append(game_state)
+    def pop(self):
+        return self.stack.pop()
 
-
+    def push(self, game_state):
+        self.stack.append(game_state)
