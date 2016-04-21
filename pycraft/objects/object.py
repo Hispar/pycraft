5dcdac2c1df8953cfe6b268bf9f4d5adce2d8514
pburgos <pburgos@corpora360.com>
2016-04-21 18:43:54 +0200

    Move pad to world object

--- pycraft/objects/object.py
+++ pycraft/objects/object.py
@@ -5,3 +5,8 @@ class WorldObject:
     breakable = False
     texture_path = None
     durability = 1
+    # How much overlap with a dimension of a surrounding block you need to
+    # have to count as a collision. If 0, touching terrain at all counts as
+    # a collision. If .49, you sink into the ground, as if walking through
+    # tall grass. If >= .5, you'll fall through the ground.
+    pad = 0.25
